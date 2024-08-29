8eb6d094be0f7b6433f2f30bd43b9e44f148537d
mdyeaminmondo <mdyeaminmondol538@gmail.com>
2024-08-29 23:31:14 +0600

    varible problem -

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 class Simple {
     public static void main(String[] args) {
         byte a = 10;
-        byte b = 10;
-        byte c = (byte) (a + b);
+        byte b = 20;
+        byte c = (byte) (a - b);
         System.out.println(c);
     }
 }
